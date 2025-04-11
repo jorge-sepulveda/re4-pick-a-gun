dc12c5b0cfb66fb321705f0a74db5c1a17c92f5b
Jorge Sepulveda <gSepulveda9697@gmail.com>
2025-04-11 17:29:15 -0500

    modified CLI for the new API changes

--- cli/cli.go
+++ cli/cli.go
@@ -10,20 +10,51 @@ import (
 func main() {
 	var sd core.SaveData
 	var option string
+	var ready = false
 
-	//err := sd.StartGame("L", core.Handguns, core.Shotguns, core.Rifles, core.Subs, core.Magnums)
-	err := sd.StartGame("A", core.AdaHandguns, core.AdaShotguns, core.AdaRifles, core.AdaSubs, core.AdaSpecials)
-	if err != nil {
-		print(err.Error() + "\n")
-		os.Exit(1)
-	}
 	fmt.Println("Welcome to the RE4 Pick a gun service!")
+	fmt.Println("Press l or a to start Leon's or Ada's playthrough or h for help")
+
+	for {
+		fmt.Scan(&option)
+		fmt.Println("Choose your option, Stranger: ")
+		switch option {
+		case string('l'):
+			fmt.Println("Starting leon's playthrough...")
+			err := sd.StartGame("L", core.Handguns, core.Shotguns, core.Rifles, core.Subs, core.Magnums)
+			if err != nil {
+				print(err.Error() + "\n")
+				os.Exit(1)
+			}
+			ready = true
+		case string('a'):
+			fmt.Println("Starting Ada's playthrough...")
+			err := sd.StartGame("A", core.AdaHandguns, core.AdaShotguns, core.AdaRifles, core.AdaSubs, core.AdaSpecials)
+			if err != nil {
+				print(err.Error() + "\n")
+				os.Exit(1)
+			}
+			ready = true
+		case string('q'):
+			fmt.Println("Quit command sent.")
+			os.Exit(0)
+		case string('h'):
+			fmt.Println("Printing Help.")
+			fmt.Println("a to roll for Seperate Ways")
+			fmt.Println("l to roll Main playthrough")
+			fmt.Println("q to quit app")
+		}
+		if ready {
+			break
+		}
+	}
+
 	fmt.Printf("Starting in Chapter %d\n", sd.CurrentChapter)
 	fmt.Printf("Current Gun: %s\n", sd.CurrentGun)
 	fmt.Printf("Used guns list %+v\n", sd.UsedGuns)
 	fmt.Printf("existing guns list %v\n", sd.GunsList)
 	for {
-		fmt.Println("Choose your weapon, Stranger: ")
+		fmt.Println("Choose your weapon, Stranger: (h for help)")
 		fmt.Scan(&option)
 		switch option {
 		case string('q'):
@@ -35,7 +66,7 @@ func main() {
 			sd.PrintData()
 		case string('l'):
 			fmt.Println("Load command sent.")
-			err = sd.LoadGame()
+			err := sd.LoadGame()
 			if err != nil {
 				print(err.Error() + "\n")
 			}
