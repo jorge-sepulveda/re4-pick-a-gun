9ce4a62da9305968dec4b3d5b9ff0d2146ee33e6
Jorge Sepulveda <gSepulveda9697@gmail.com>
2025-04-22 20:13:27 -0500

    added POST method for loading data
    Need to update core.go to handle the load function.

--- comms/server.go
+++ comms/server.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"io"
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/jorge-sepulveda/re4-pick-a-gun/core"
-	"net/http"
 )
 
 func main() {
@@ -12,6 +14,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/start", PingHandler).Methods("GET")
 	r.HandleFunc("/roll", rollHandler).Methods("POST")
+	r.HandleFunc("/load", loadHandler).Methods("POST")
 	http.ListenAndServe(":8080", r)
 
 }
@@ -25,14 +28,14 @@ func PingHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func rollHandler(w http.ResponseWriter, r *http.Request) {
-	var sd core.SaveData
+	sd := core.SaveData{}
 	err := json.NewDecoder(r.Body).Decode(&sd)
 	if err != nil {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
 	if sd.CurrentChapter == sd.FinalChapter {
-		http.Error(w, "All out of chapters, stranger!", http.StatusBadRequest)
+		http.Error(w, "All out of chapters, stranger!", http.StatusOK)
 		return
 
 	}
@@ -41,4 +44,14 @@ func rollHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(sd)
 }
 
-func loadHandler(w http.ResponseWriter)
+func loadHandler(w http.ResponseWriter, r *http.Request) {
+	sd := core.SaveData{}
+	requestBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Invalid Payload Data, unable to load", http.StatusBadRequest)
+	}
+	defer r.Body.Close()
+	sd.LoadString(requestBytes)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(sd)
+}
