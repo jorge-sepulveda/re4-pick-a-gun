b49324f4719b8683b2964dde1ab9d36427d89aab
Jorge Sepulveda <gSepulveda9697@gmail.com>
2025-05-07 13:49:33 -0500

    added comms for simple HTTP Server

--- comms/go.mod
+++ comms/go.mod
@@ -3,6 +3,6 @@ module github.com/jorge-sepulveda/re4-pick-a-gun/comms
 go 1.21.5
 
 require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/jorge-sepulveda/re4-pick-a-gun/core v1.0.1 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/jorge-sepulveda/re4-pick-a-gun/core v1.0.1
 )
--- comms/server.go
+++ comms/server.go
@@ -3,25 +3,63 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"log/slog"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/jorge-sepulveda/re4-pick-a-gun/core"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 	r := mux.NewRouter()
-	r.HandleFunc("/start", PingHandler).Methods("GET")
+	r.Use(LoggingMiddleware(logger))
+	r.HandleFunc("/start", StartHandler).Methods("GET")
 	r.HandleFunc("/roll", rollHandler).Methods("POST")
 	r.HandleFunc("/load", loadHandler).Methods("POST")
+	logger.Info("starting server", "port", 8080)
 	http.ListenAndServe(":8080", r)
 
 }
 
-func PingHandler(w http.ResponseWriter, r *http.Request) {
-	sd := core.SaveData{}
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
 
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func LoggingMiddleware(logger *slog.Logger) mux.MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+			next.ServeHTTP(rw, r)
+
+			duration := time.Since(start).Milliseconds()
+
+			logger.Info("api_request", slog.Group("request",
+				slog.String("method", r.Method),
+				slog.String("path", r.URL.Path),
+				slog.Int("status", rw.statusCode),
+				slog.Int64("duration_ms", duration),
+				slog.String("client_ip", r.RemoteAddr),
+				slog.Time("timestamp", time.Now()),
+			))
+
+		})
+	}
+}
+
+func StartHandler(w http.ResponseWriter, r *http.Request) {
+	sd := core.SaveData{}
 	sd.StartGame("L", core.Handguns, core.Shotguns, core.Rifles, core.Subs, core.Magnums)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(sd)
